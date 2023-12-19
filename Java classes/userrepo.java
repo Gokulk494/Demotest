c636e135bf1c881a875565de4d99e318baa8765e
Gokulk494 <130535132+Gokulk494@users.noreply.github.com>
2023-12-19 23:37:12 +0530

    Delete Java classes/userrepo.java

--- Java classes/userrepo.java
+++
@@ -1,8 +0,0 @@
-package eight.repository;
-
-import eight.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
