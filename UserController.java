b038d617a51ae1065dea9f19483f2943293ca365
Gokulk494 <130535132+Gokulk494@users.noreply.github.com>
2023-12-19 23:44:52 +0530

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,1 +1,30 @@
+package eight.controller;
+
+import eight.model.User;
+import eight.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/index")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "index";
+    }
+
+    @PostMapping("/index")
+    public String processRegistration(User user) {
+        userRepository.save(user);
+        // Redirect to login page after registration
+        return "redirect:/login";
+    }
+}
 
