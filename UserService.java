898faab30164bb61a9bbe9a7030f925966b5c5c3
Gokulk494 <130535132+Gokulk494@users.noreply.github.com>
2024-02-01 21:05:05 +0530

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -1,1 +1,29 @@
+// UserService.java
+package eight.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import eight.model.User;
+import eight.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User loginUser(String username, String password) {
+        return userRepository.findByUsernameAndPassword(username, password);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    // Add additional service methods if needed
+}
 
