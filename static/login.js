f8924edd6730283093b0c3e46e0e31ba813ae228
Gokulk494 <130535132+Gokulk494@users.noreply.github.com>
2024-02-01 21:09:20 +0530

    Update login.js

--- static/login.js
+++ static/login.js
@@ -1,1 +1,48 @@
+// src/main/resources/static/js/login.js
+
+function performLogin() {
+    var gmail = $('#gmail').val();
+    var password = $('#password').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/login',
+        data: {
+            gmail: gmail,
+            password: password
+        },
+        success: function (data) {
+            if (data.hasOwnProperty('success')) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Successful!',
+                    text: 'Welcome back!',
+                    confirmButtonColor: '#3085d6',
+                    confirmButtonText: 'OK'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = '/profile';
+                    }
+                });
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Failed',
+                    text: data.error,
+                    confirmButtonColor: '#3085d6',
+                    confirmButtonText: 'OK'
+                });
+            }
+        },
+        error: function () {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'An unexpected error occurred',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'OK'
+            });
+        }
+    });
+}
 
